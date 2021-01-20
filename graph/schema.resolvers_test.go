cb3e2a4674bc8be154862e5b4469932427c242f1
thinhnguyenv <thinh.nguyenv@s3corp.com.vn>
2021-01-20 15:32:54 +0700

    Unit test not through HttpRequest.

--- graph/schema.resolvers_test.go
+++ graph/schema.resolvers_test.go
@@ -1,15 +1,12 @@
 package graph
 
 import (
+	"context"
 	"testing"
 
-	"S3_FriendManagement_Graphql/graph/generated"
 	"S3_FriendManagement_Graphql/graph/graphqlmodels"
 	"S3_FriendManagement_Graphql/modelss/api-models"
-	"github.com/99designs/gqlgen/client"
-	"github.com/99designs/gqlgen/handler"
 	"github.com/friendsofgo/errors"
-	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,116 +16,55 @@ func TestMutationResolver_CreateUser(t *testing.T) {
 		result bool
 		err    error
 	}
+
 	type mockCreateUserService struct {
 		input  *api_models.UserServiceInput
 		result int
 		err    error
 	}
+
 	testCases := []struct {
 		name                  string
-		query                 string
+		input                 graphqlmodels.NewUser
 		mockIsUserExisted     mockIsUserExisted
 		mockCreateUserService mockCreateUserService
 		expectedResult        *graphqlmodels.User
-		expectedErr           error
+		expectedError         error
 	}{
 		{
-			name: "Format failed",
-			query: `mutation {
-					  createUser(input: {
-						  email: "abcgmail.com"
-					  }){
-						  id,
-						  email
-					  }
-					}`,
-			expectedResult: &graphqlmodels.User{
-				ID:    1,
-				Email: "abc@gmail.com",
-			},
-			expectedErr: errors.New("[{\"message\":\"\\\"email\\\"'s format is not valid. (ex: \\\"andy@abc.xyz\\\")\",\"path\":[\"createUser\"]}]"),
-		},
-		{
-			name: "Existed user",
-			query: `mutation {
-					  createUser(input: {
-						  email: "abc@mail.com"
-					  }){
-						  id,
-						  email
-					  }
-					}`,
-			mockIsUserExisted: mockIsUserExisted{
-				input:  "abc@mail.com",
-				result: true,
-				err:    nil,
+			name: "Failed",
+			input: graphqlmodels.NewUser{
+				Email: "gmail.com",
 			},
 			expectedResult: nil,
-			expectedErr:    errors.New("[{\"message\":\"this email address existed\",\"path\":[\"createUser\"]}]"),
-		},
-		{
-			name: "Success",
-			query: `mutation {
-					  createUser(input: {
-						  email: "abc@gmail.com"
-					  }){
-						  id,
-						  email
-					  }
-					}`,
-			mockIsUserExisted: mockIsUserExisted{
-				input:  "abc@gmail.com",
-				result: false,
-				err:    nil,
-			},
-			mockCreateUserService: mockCreateUserService{
-				input: &api_models.UserServiceInput{
-					Email: "abc@gmail.com",
-				},
-				result: 1,
-				err:    nil,
-			},
-			expectedResult: &graphqlmodels.User{
-				ID:    1,
-				Email: "abc@gmail.com",
-			},
-			expectedErr: nil,
+			expectedError:  errors.New("\"email\"'s format is not valid. (ex: \"andy@abc.xyz\")"),
 		},
 	}
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			//Given
+			ctx := context.Background()
+
 			mockUserService := new(mockUserService)
-			mockFriendService := new(mockFriendService)
-			mockSubscriptionService := new(mockSubscriptionService)
-			mockBlockingService := new(mockBlockingService)
 
 			mockUserService.On("IsExistedUser", testCase.mockIsUserExisted.input).
 				Return(testCase.mockIsUserExisted.result, testCase.mockIsUserExisted.err)
 			mockUserService.On("CreateUser", testCase.mockCreateUserService.input).
 				Return(testCase.mockCreateUserService.result, testCase.mockCreateUserService.err)
 
-			graphqlServer := handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &Resolver{
-				IUserService:         mockUserService,
-				IFriendService:       mockFriendService,
-				ISubscriptionService: mockSubscriptionService,
-				IBlockingService:     mockBlockingService,
-			}}))
+			r := Resolver{
+				IUserService: mockUserService,
+			}
+			mut := r.Mutation()
 
-			testClient := client.New(graphqlServer)
-			result := &graphqlmodels.User{}
-			if testCase.expectedErr != nil {
-				err := testClient.Post(testCase.query, &result)
-				require.EqualError(t, err, testCase.expectedErr.Error())
+			//When
+			result, err := mut.CreateUser(ctx, testCase.input)
+
+			//Then
+			if testCase.expectedError != nil {
+				require.EqualError(t, err, testCase.expectedError.Error())
 			} else {
-				resp, err := testClient.RawPost(testCase.query)
-				if err != nil {
-					t.Error(err)
-				}
-				if err := mapstructure.Decode(resp.Data.(map[string]interface{})["createUser"], &result); err != nil {
-					t.Error(err)
-				}
-				require.EqualValues(t, testCase.expectedResult, result)
+				require.Equal(t, testCase.expectedResult, result)
 			}
 		})
 	}
